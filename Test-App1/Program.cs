912e6d1ec7a7cb57585490ae303fc8ba097570cc
Francisco M. <fmoll1999@gmail.com>
2026-04-23 12:08:57 -0400

    Made a gray box window

--- Test-App1/Program.cs
+++ Test-App1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+
+namespace Blocks {
+    class App : Game {
+        private readonly GraphicsDeviceManager graphics;
+        public App() {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.DimGray);
+        }
+        protected static void Main() {
+            using var app = new App();
+            app.Run();
+        }
+    }
+}
